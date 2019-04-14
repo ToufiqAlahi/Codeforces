ac165c9760b9dee2d1c3ff4b910690c2a4083b71
Sheikh Deep <30627010+sheikhDeep@users.noreply.github.com>
2019-04-14 13:28:44 +0600

    Add files via upload

---
+++ Round_551_B.cpp
@@ -0,0 +1,101 @@
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+#define endl '\n'
+#define PB push_back
+#define MP make_pair
+#define LL long long
+#define FOR(i,a,b) for(int i = (a); i <= (b); i++)
+#define RE(i,n) FOR(i,1,n)
+#define REP(i,n) FOR(i,0,(int)(n)-1)
+#define R(i,n) REP(i,n)
+#define VI vector<int>
+#define PII pair<int,int>
+#define LD long double
+#define FI first
+#define SE second
+#define st FI
+#define nd SE
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+
+typedef long long ll;
+
+bool file=0,rt=1;
+clock_t tStart ;
+void FAST_IO();
+////////////////////////
+
+
+
+
+
+int f[101];
+int l[101];
+int main() {
+
+    //FAST_IO();
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout << setprecision(5);
+    cout << fixed;
+
+    int n, m, h;
+
+    cin >> n >> m >> h;
+
+    for(int i = 0; i < m; i++) {
+
+        cin >> f[i];
+    }
+    for(int i = 0; i < n; i++) {
+
+        cin >> l[i];
+    }
+
+
+
+    for(int i = 0; i < n; i++) {
+
+        for(int j = 0; j < m; j++) {
+
+            int x;
+            cin >> x;
+
+            if(x == 0) {
+
+                cout << 0 << " ";
+            }
+            else {
+
+                cout << min(f[j], l[i]) << " ";
+            }
+        }
+        cout << endl;
+    }
+
+
+
+    return 0;
+}
+
+void FAST_IO()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    //cout.tie(NULL);
+    //cout.setf(ios::fixed);
+    //cout.precision(2);
+    if(rt && file){ tStart = clock(); }
+    if(file){
+    #ifndef _offline
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    }
+}
+
+
